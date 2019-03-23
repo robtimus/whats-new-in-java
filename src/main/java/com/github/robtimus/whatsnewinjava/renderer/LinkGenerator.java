e567bef95e826d479956a02019b832b020bbd3a5
Rob Spoor <rob@icemanx.nl>
2019-03-23 19:59:42 +0100

    The latest Java API base URL is no longer repeated for all links

--- src/main/java/com/github/robtimus/whatsnewinjava/renderer/LinkGenerator.java
+++ src/main/java/com/github/robtimus/whatsnewinjava/renderer/LinkGenerator.java
@@ -12,31 +12,41 @@ import com.github.robtimus.whatsnewinjava.domain.JavaVersion;
 final class LinkGenerator {
 
     private final NavigableMap<JavaVersion, JavaAPI> javaAPIs;
+    private final String latestBaseURL;
 
     LinkGenerator(NavigableMap<JavaVersion, JavaAPI> javaAPIs) {
         this.javaAPIs = javaAPIs;
+        latestBaseURL = javaAPIs.isEmpty() ? "" : javaAPIs.lastEntry().getValue().getJavadoc().getBaseURL();
+    }
+
+    public String getLatestBaseURL() {
+        return latestBaseURL;
     }
 
     public String generateLink(JavaModule javaModule) {
         JavaModule latestModule = findLatestModule(javaModule);
         String format = latestModule.getJavadoc().useModules() ? "%s%s/module-summary.html" : "%s%s-summary.html";
-        return String.format(format, latestModule.getJavadoc().getBaseURL(), javaModule.getName());
+        return String.format(format, latestModule.getJavadoc().getBaseURL(), javaModule.getName())
+                .replace(latestBaseURL, "");
     }
 
     public String generateLink(JavaPackage javaPackage) {
         JavaPackage latestPackage = findLatestPackage(javaPackage);
-        return String.format("%s%s/package-summary.html", latestPackage.getJavadoc().getBaseURL(), getRelativeBaseURL(latestPackage));
+        return String.format("%s%s/package-summary.html", latestPackage.getJavadoc().getBaseURL(), getRelativeBaseURL(latestPackage))
+                .replace(latestBaseURL, "");
     }
 
     public String generateLink(JavaClass javaClass) {
         JavaClass latestClass = findLatestClass(javaClass);
-        return String.format("%s%s/%s.html", latestClass.getJavadoc().getBaseURL(), getRelativeBaseURL(latestClass.getJavaPackage()), latestClass.getName());
+        return String.format("%s%s/%s.html", latestClass.getJavadoc().getBaseURL(), getRelativeBaseURL(latestClass.getJavaPackage()), latestClass.getName())
+                .replace(latestBaseURL, "");
     }
 
     public String generateLink(JavaMember javaMember) {
         JavaMember latestMember = findLatestMember(javaMember);
         JavaClass latestClass = latestMember.getJavaClass();
-        return String.format("%s%s/%s.html#%s", latestClass.getJavadoc().getBaseURL(), getRelativeBaseURL(latestClass.getJavaPackage()), latestClass.getName(), latestMember.getOriginalSignature());
+        return String.format("%s%s/%s.html#%s", latestClass.getJavadoc().getBaseURL(), getRelativeBaseURL(latestClass.getJavaPackage()), latestClass.getName(), latestMember.getOriginalSignature())
+                .replace(latestBaseURL, "");
     }
 
     private String getRelativeBaseURL(JavaPackage javaPackage) {
